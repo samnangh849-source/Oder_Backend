c5ba576ebf2efedc3f28aada4f761957cd73fec0
samnangh849-source <samnangh849@gmail.com>
2026-05-05 17:06:15 +0700

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.acc.ordersystem',
-  appName: 'ACC Order System',
-  webDir: 'dist'
-};
-
-export default config;
