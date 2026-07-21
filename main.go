72bc252ede06546c6b3a1693a9c5f60c1ed3bf77
samnangh849-source <samnangh849@gmail.com>
2026-07-21 16:09:10 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1624,7 +1624,7 @@ func handleGetAllOrders(c *gin.Context) {
 	// Field Selection
 	if view == "compact" {
 		// Include products_json as it's needed for the dashboard display
-		query = query.Select("order_id, timestamp, user, page, telegram_value, customer_name, customer_phone, location, address_details, note, shipping_fee_customer, subtotal, grand_total, internal_shipping_method, internal_shipping_details, internal_cost, payment_status, payment_info, discount_usd, delivery_unpaid, delivery_paid, total_product_cost, telegram_message_id1, telegram_message_id2, telegram_message_id3, scheduled_time, fulfillment_store, team, is_verified, fulfillment_status, packed_by, packed_time, package_photo_url, driver_name, tracking_number, dispatched_time, dispatched_by, delivered_time, delivery_photo_url, products_json")
+		query = query.Select("order_id, timestamp, user, page, telegram_value, customer_name, customer_phone, location, address_details, note, shipping_fee_customer, subtotal, grand_total, internal_shipping_method, internal_shipping_details, internal_cost, payment_status, payment_info, discount_usd, delivery_unpaid, delivery_paid, total_product_cost, telegram_message_id1, telegram_message_id2, telegram_message_id3, scheduled_time, fulfillment_store, team, is_verified, fulfillment_status, packed_by, packed_time, package_photo_url, driver_name, tracking_number, dispatched_time, dispatched_by, delivered_time, delivery_photo_url, products_json, cancel_reason, return_reason, return_photo_url, return_received_by, return_received_time")
 	}
 
 	// Apply Pagination
